0f93266856a842ffdfdc348d13acba0af6952b36
Valentin GAY <mystogan40@gmail.com>
2020-01-31 07:05:23 +0100

    Add display of cart items in dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './cart-dropdown.styles.scss';
 import { CustomButton } from '../index';
+import { CartItem } from '../index';
 
-export const CartDropdown = () => (
-  <div className="cart-dropdown">
-    <div className="cart-items" />
-    <CustomButton>GO TO CHECKOUT </CustomButton>
-  </div>
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export const CartDropdown = connect(mapStateToProps)(
+  ({ cartItems }) => (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {
+          cartItems.map(item => (
+            <CartItem key={item.id} item={item} />
+          ))
+        }
+      </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+  )
 );
