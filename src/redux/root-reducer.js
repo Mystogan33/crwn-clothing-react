55cc8fc7114c4dce56bfd545a7663b263b527519
Valentin GAY <mystogan40@gmail.com>
2020-01-31 11:39:02 +0100

    Add Redux Persist to persist state of the app

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import headerReducer from './header/header.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart', 'header']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   header: headerReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
