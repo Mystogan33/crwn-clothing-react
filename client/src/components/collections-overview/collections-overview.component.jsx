b36c0d2cf20393814ec3942c140da59069a4a06c
Valentin GAY <mystogan40@gmail.com>
2020-03-02 03:45:21 +0100

    the app is ready for production, unit tests works so ready to deploy

--- client/src/components/collections-overview/collections-overview.component.jsx
+++ client/src/components/collections-overview/collections-overview.component.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectShopCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import { CollectionPreview } from '../collection-preview/collection-preview.component';
 import { CollectionsOverviewContainer } from './collections-overview.styles';
 
-export const CollectionsOverview = ({collections}) => (
+export const CollectionsOverview = ({ collections }) => (
   <CollectionsOverviewContainer>
     { collections.map(({id, ...otherProps }) => (
         <CollectionPreview key={id} {...otherProps} />
@@ -14,18 +11,3 @@ export const CollectionsOverview = ({collections}) => (
     }
   </CollectionsOverviewContainer>
 );
-
-// export const CollectionsOverviewComponent = ({collections}) => (
-//   <CollectionsOverviewContainer>
-//     { collections.map(({id, ...otherProps }) => (
-//         <CollectionPreview key={id} {...otherProps} />
-//       ))
-//     }
-//   </CollectionsOverviewContainer>
-// );
-
-// const mapStateToProps = createStructuredSelector({
-//   collections: selectShopCollectionsForPreview
-// });
-
-// export const CollectionsOverview = connect(mapStateToProps)(CollectionsOverviewComponent);
