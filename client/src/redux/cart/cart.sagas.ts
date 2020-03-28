efc5167d6a1c035e5515094104f5b23df2d23dcc
Valentin GAY <mystogan40@gmail.com>
2020-03-28 19:18:53 +0100

    convert all files to ts/tsx, at this point everything is broken [NOT STABLE]

--- client/src/redux/cart/cart.sagas.ts
+++ client/src/redux/cart/cart.sagas.ts
@@ -2,7 +2,7 @@ import { all, call, takeLatest, put, select } from 'redux-saga/effects';
 
 import UserActionTypes from '../user/user.types';
 import { selectCurrentUser } from '../user/user.selectors';
-import { CartActionTypes } from '../cart/cart.types';
+import { CartActionTypes } from './cart.types';
 import { selectCartItems } from './cart.selectors';
 import { clearCart, setCartFromFirebase } from './cart.actions';
 
