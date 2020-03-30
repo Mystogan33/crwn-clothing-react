7a78ab60a82283c2f29b9b15a97a3cb79cd985e4
Valentin GAY <mystogan40@gmail.com>
2020-03-30 11:58:47 +0200

    App and index files successfully converts to TSX

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { connect, ConnectedProps } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -9,13 +9,26 @@ import { checkUserSession } from './redux/user/user.actions';
 import { GlobalStyle } from './global.styles';
 
 import { Header, Spinner, ErrorBoundary } from './components';
+import { Dispatch } from 'redux';
 
 const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
 const ShopPage = lazy(() => import('./pages/shop/shop.component'));
 const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
 const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
 
-const App = ({ checkUserSession, currentUser }) => {
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  checkUserSession: () => dispatch(checkUserSession())
+});
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type ReduxProps = ConnectedProps<typeof connector>;
+type AppProps = ReduxProps;
+
+const App = ({ checkUserSession, currentUser }: AppProps) => {
 
   useEffect(() => {
     checkUserSession();
@@ -45,12 +58,4 @@ const App = ({ checkUserSession, currentUser }) => {
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
-});
-
-const mapDispatchToProps = dispatch => ({
-  checkUserSession: () => dispatch(checkUserSession())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connector(App);
